f70db096245cc551265b4fa97cf2fd2eb4b30398
“weiyige” <weiyige@xverse.cn>
2026-03-06 11:16:30 +0800

    feat: 更新prettierrc

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <body>{children}</body>
     </html>
   );
