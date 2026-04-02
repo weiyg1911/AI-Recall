789ee63fee86ee285c51480b72e0a98c94734674
weiyige <weiyige@xverse.cn>
2026-04-02 23:21:29 +0800

    feat: 在configuration中去除不必要的数据库字段

--- apps/backend/src/common/config/configuration.ts
+++ apps/backend/src/common/config/configuration.ts
@@ -7,10 +7,6 @@ export default () => ({
   },
   port: parseInt(process.env.PORT ?? '3000', 10),
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
     uri: process.env.MONGODB_URI,
   },
   redis: {
